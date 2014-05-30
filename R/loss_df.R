35a8ed241a16b6a3af51a3d4e2790ce80aa34695
merlinoa88@gmail.com <merlinoa88@gmail.com>
2014-05-30 08:48:05 -0400

    plotting improvements

--- R/loss_df.R
+++ R/loss_df.R
@@ -37,7 +37,8 @@
 #'             reserve_amount = sum(reserve_amount),
 #'             origin = mean(origin),
 #'             paid_expense = sum(X4_exp_payment),
-#'             incurred_expense = sum(X4_exp_reserve)
+#'             incurred_expense = sum(X4_exp_reserve),
+#'             sal_sub = sum(payment_no_reserve_a)
 #'            )
 #' occurences <- mutate(occurences,
 #'                      paid_loss_only = payment_amount - paid_expense,
@@ -52,7 +53,7 @@
 #'                              evaluation_date = "evaluation_date", 
 #'                              paid = c("paid_loss_only", "paid_expense"),
 #'                              incurred = c("incurred_loss_only", "incurred_expense"),
-#'                              paid_recovery = "paid_excess250",
+#'                              paid_recovery = c("sal_sub", "paid_excess250"),
 #'                              incurred_recovery = "incurred_excess250",
 #'                              desc = "claim_cts"
 #'                  )
@@ -60,7 +61,7 @@
 #' # use numbers instead of character vector to identify columns
 #' mydf2 <- loss_df(losses2, id = 1, origin = 7, dev = 2, evaluation_date = 3,
 #'                  paid = c(8, 10), incurred = c(9, 11), desc = 4)
-loss_df <- function(df, id, origin, dev, evaluation_date = NULL, paid = NULL, 
+loss_df <- function(ldf, id, origin, dev, evaluation_date = NULL, paid = NULL, 
                     incurred = NULL, paid_recovery = NULL, incurred_recovery = NULL,
                     desc = NULL) {
   # create list with bin for each attribute 'type'
@@ -74,11 +75,11 @@ loss_df <- function(df, id, origin, dev, evaluation_date = NULL, paid = NULL,
                  incurred_recovery = incurred_recovery,
                  desc = desc
                  )
-  values <- lapply(values, num_to_name, df = df)
+  values <- lapply(values, num_to_name, df = ldf)
   #change relevant columns to new names
-  relevant <- match(unlist(values), colnames(df))
+  relevant <- match(unlist(values), colnames(ldf))
   relevant <- relevant[!is.na(relevant)]
-  df2 <- df[, relevant]
+  df2 <- ldf[, relevant]
   # applying attributes
   n <- unlist(lapply(values, length))
   atr <- list()
@@ -102,7 +103,7 @@ is.loss_df <- function(x) inherits(x, "loss_df")
 
 #' get the sum of loss_df data by origin period
 #' 
-#' @param loss_df loss_df S3 object
+#' @param ldf loss_df S3 object
 #' 
 #' @keywords internal
 #' @method summary loss_df
@@ -115,32 +116,32 @@ is.loss_df <- function(x) inherits(x, "loss_df")
 #' 
 #' # with specified `evaluation_date`
 #' summary(losses_loss_df, evaluation_date = "2012-06-30")
-summary.loss_df <- function(df, evaluation_date = NULL) {
-  df$calendar <- get_col(df_ = df, type = "origin") +
-                 get_col(df_ = df, type = "dev")
-  cols <- get_colnum(df_ = df, type = c("id", "dev"))
-  df2 <- df[, -cols]
-  df2 <- carry_attr(df1 = df, df2 = df2)
+summary.loss_df <- function(ldf, evaluation_date = NULL) {
+  ldf$calendar <- get_col(df = ldf, type = "origin") +
+                 get_col(df = ldf, type = "dev")
+  cols <- get_colnum(df = ldf, type = c("id", "dev"))
+  df2 <- ldf[, -cols]
+  df2 <- carry_attr(df1 = ldf, df2 = df2)
   attr(df2, "type")[length(attr(df2, "type"))] <- "calendar"
   if (is.null(evaluation_date)){
-    latest <- df2[df2$calendar == max(df2$calendar), -get_colnum(df_ = df2, type = "evaluation_date")]
+    latest <- df2[df2$calendar == max(df2$calendar), -get_colnum(df = df2, type = "evaluation_date")]
     latest <- carry_attr(df1 = df2, df2 = latest)
-    latest_no_origin <- latest[, -get_colnum(df_ = latest, type = "origin")]
+    latest_no_origin <- latest[, -get_colnum(df = latest, type = "origin")]
     smry <- apply(latest_no_origin, 2,
-                  function(x) tapply(x, get_col(df_ = latest, type = "origin"), sum, na.rm = TRUE))
+                  function(x) tapply(x, get_col(df = latest, type = "origin"), sum, na.rm = TRUE))
   } else {
-    selected <- df2[get_col(df_ = df2, type = "evaluation_date") == evaluation_date, 
-                    -get_colnum(df_ = df2, type = "evaluation_date")]
+    selected <- df2[get_col(df = df2, type = "evaluation_date") == evaluation_date, 
+                    -get_colnum(df = df2, type = "evaluation_date")]
     selected <- carry_attr(df1 = df2, df2 = selected)
-    selected_no_origin <- selected[, -get_colnum(df_ = selected, type = "origin")]
-    smry <- apply(selected[, -which(names(selected) %in% get_colname(df_ = selected, type = "origin"))], 2,
-                  function(x) tapply(x, get_col(df_ = selected, type = "origin"), sum, na.rm= TRUE))
+    selected_no_origin <- selected[, -get_colnum(df = selected, type = "origin")]
+    smry <- apply(selected[, -which(names(selected) %in% get_colname(df = selected, type = "origin"))], 2,
+                  function(x) tapply(x, get_col(df = selected, type = "origin"), sum, na.rm= TRUE))
   }
   smry <- as.data.frame(smry[, -which(colnames(smry) == "calendar")])
   
   attr(smry, "eval") <- evaluation_date
   origin <- data.frame(rownames(smry))
-  names(origin) <- get_colname(df_ = df, type = "origin")
+  names(origin) <- get_colname(df = ldf, type = "origin")
   smry <- cbind(origin, smry)
   rownames(smry) <- NULL
   smry
@@ -165,15 +166,15 @@ summary.loss_df <- function(df, evaluation_date = NULL) {
 #' # plot paid and case at selected `evaluation_date`
 #' plot(losses_loss_df, evaluation_date = "2012-06-30")
 #' ## need to fix so other evaluation dates besides latest work
-plot.loss_df <- function(df, evaluation_date = NULL) {
+plot.loss_df <- function(ldf, evaluation_date = NULL) {
   # format data frame
   if (is.null(evaluation_date)) {
-    smry <- as.data.frame(summary(df))
+    smry <- as.data.frame(summary(ldf))
   } else {
-    smry <- as.data.frame(summary(df, evaluation_date = evaluation_date))
+    smry <- as.data.frame(summary(ldf, evaluation_date = evaluation_date))
   } 
-  smry <- carry_attr(df1 = df, df2 = smry)
-  pdata <- data.frame(get_col(df_ = smry, type = "origin"))
+  smry <- carry_attr(df1 = ldf, df2 = smry)
+  pdata <- data.frame(get_col(df = smry, type = "origin"))
   smry$incurred <- sum_type(df = smry, type = "incurred")
   smry$incurred_recovery <- -sum_type(df = smry, type = "incurred_recovery")
   pdata$paid_recovery <- -sum_type(df = smry, type = "paid_recovery")
@@ -183,8 +184,9 @@ plot.loss_df <- function(df, evaluation_date = NULL) {
   
   totals <- melt(pdata, id.vars = 1)
   
-  # separate data frames based on positive whether value is positive or negative
-  # this is necessary to create stacked ggplot bar chart with positive and neg values
+  # separate data frames for positive or negative value
+  # this is necessary to create stacked ggplot bar chart with 
+  # positive and neg values in same stack
   pos <- subset(totals, value > 0)
   neg <- subset(totals, value < 0)
   
@@ -193,33 +195,33 @@ plot.loss_df <- function(df, evaluation_date = NULL) {
   
   # create plot
   p <- ggplot() +
-    geom_bar(data = pos, aes_string(x = get_colname(df_ = pos, type = "origin"), y = "value", fill = "variable"),
+    geom_bar(data = pos, aes_string(x = get_colname(df = pos, type = "origin"), y = "value", fill = "variable"),
              stat = "identity") +
-    geom_bar(data = neg, aes_string(x = get_colname(df_ = neg, type = "origin"), y = "value", fill = "variable"),
+    geom_bar(data = neg, aes_string(x = get_colname(df = neg, type = "origin"), y = "value", fill = "variable"),
              stat = "identity") +
-  xlab("Origin Year") + ylab("Loss Amounts") + 
-  ggtitle("Loss Amounts by Origin Year")
-  p
+    xlab("Origin Year") + ylab("Loss Amounts") + 
+    ggtitle("Loss Amounts by Origin Year")
+  suppressWarnings(print(p))
 }
 
 
 #' check that arguments and data columns to loss_df are appropriate
 #' 
 #' @param df a loss_df to check
-check_loss_df <- function(df) {
+check_loss_df <- function(ldf) {
   ## need to add more checks
   # check that id, origin and dev are each 1 column
-  if (length(get_colname(df_ = df, c("id", "origin", "dev"))) != 3) {
+  if (length(get_colname(df = ldf, c("id", "origin", "dev"))) != 3) {
     stop("id, origin, and dev can only reference 1 column each")
   }
   # check to see that at least some loss data supplied
-  if (length(get_colname(df_ = df, c("paid", "incurred", "paid_recovery",
+  if (length(get_colname(df = ldf, c("paid", "incurred", "paid_recovery",
                                       "incurred_recovery", "desc"))) == 0) {
     stop("Some loss data must be provided")
   }
-  factor_cols <- get_colname(df_ = df, c("id", "evaluation_date"))
-  numeric_cols <- get_colname(df_ = df, c("paid", "incurred", "paid_recovery",
+  factor_cols <- get_colname(df = ldf, c("id", "evaluation_date"))
+  numeric_cols <- get_colname(df = ldf, c("paid", "incurred", "paid_recovery",
                                            "incurred_recovery", "desc"))
-  if (!all(unlist(lapply(df[, factor_cols], is.factor)))) stop("set 'id' and 'evaluation_date' to factor")
-  if (!all(unlist(lapply(df[, numeric_cols], is.numeric)))) stop("All columns other than 'id' and 'evaluation_date' must be numeric")  
+  if (!all(unlist(lapply(ldf[, factor_cols], is.factor)))) stop("set 'id' and 'evaluation_date' to factor")
+  if (!all(unlist(lapply(ldf[, numeric_cols], is.numeric)))) stop("All columns other than 'id' and 'evaluation_date' must be numeric")  
 }
