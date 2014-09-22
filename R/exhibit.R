e33a8b2ba37ee0d87bd887e8d575c3c62206ebae
merlinoa <merlinoa88@gmail.com>
2014-09-22 17:03:05 -0400

    improved comments and added glmReserve exhibit method

--- R/exhibit.R
+++ R/exhibit.R
@@ -1,27 +1,26 @@
 #' Generic similar to summary
 #' 
-#' Exhibit defines certain ways to arrange values in
-#' the \code{reserve} package for presentation in a report.
-#' It is similar to summary, but tailored to fit nicely
-#' on a single sheet of paper.  A typical result from
-#' executing the \code{exhibit} function is a data frame in
-#' which each row represents an origin period and each
-#' column represents a some aggregate claim loss amount.
+#' \code{exhibit} defines certain default ways to arrange 
+#' objects returned by the package \code{ChainLadder} package
+#' and \code{loss_db} packages for presentation in a report.  
+#' A typical result from executing the \code{exhibit} function
+#' is a data frame in which each row represents an origin 
+#' period and each column represents a some aggregate claim 
+#' loss amount.
 #' 
-#' @param data objext to turn into exhibit
+#' @param data object to turn into exhibit
 #' @param ... additional arguments
 #' 
 #' @export
 exhibit <- function(data, ...) UseMethod("exhibit")
-
-# I would like to incorporate xtable or knitr package so my tables
-# look nicer.  These are ok, but not nice enough for actual report.
 
 
 #' Return a triangle for age to age development factors
 #' 
 #' @param ata object of class ata generated from \code{ChainLadder} package
 #' @param selection optional selected development factors
+#' @param tail_column optional column for development factor of 
+#' most mature age to ultimate
 #' 
 #' @keywords internal
 #' @method exhibit ata
@@ -36,22 +35,33 @@ exhibit <- function(data, ...) UseMethod("exhibit")
 #' 
 #' # with tail factor selected
 #' exhibit(dev_tri, selection = c(1.9, 1.2, 1.13, 1.075, 1.1))
-exhibit.ata <- function(ata, selection = NULL) {
+exhibit.ata <- function(ata, selection = NULL, tail_column = FALSE) {
   
   # extract development table from ata object
   xhbt <- as.data.frame(ata[1:nrow(ata), 1:ncol(ata)])
   
-  # create name for final column
-  final_col_name <- paste0(ncol(xhbt) + 1, "-Ult.")
+  if (tail_column) {
+    # create name for final column
+    final_col_name <- paste0(ncol(xhbt) + 1, "-Ult.")
   
-  # create final column
-  final_col <- data.frame(rep(NA, times = nrow(xhbt)))
-  names(final_col) <- final_col_name
-  xhbt <- cbind(xhbt, final_col)
-  xhbt <- rbind(xhbt, 
+    # create final column
+    final_col <- data.frame(rep(NA, times = nrow(xhbt)))
+    names(final_col) <- final_col_name
+  
+    # add final column to exhibit
+    xhbt <- cbind(xhbt, final_col)
+    xhbt <- rbind(xhbt, 
                 smpl = c(attr(ata, "smpl"), NA),
                 wtd = c(attr(ata, "vwtd"), NA),
                 sel = selection)
+  } else {
+    xhbt <- rbind(xhbt,
+                  smpl = c(attr(ata, "smpl")),
+                  wtd = c(attr(ata, "vwtd")),
+                  sel = selection)
+  }
+  
+  # format the values for presentation
   format(round(xhbt, 3), digits = 3, nsmall = 3)
 }
 
@@ -81,5 +91,24 @@ exhibit.triangle <- function(tri) {
 exhibit.ratio <- function(tri) {
   xhbt <- as.data.frame(tri[1:nrow(tri), 1:ncol(tri)])
   xhbt <- format(round(xhbt, 3), digits = 3, nsmall = 3)
+  xhbt
+}
+
+
+#' Returns glmReserve summary information in a data frame
+#'
+#' @param glmReserve object of class glmReserve
+#' 
+#' @keywords internal
+#' @method exhibit glmReserve
+#' 
+#' @export
+exhibit.glmReserve <- function(glmReserve) {
+  xhbt <- glmReserve$summary
+  latest_first_ay <- glmReserve$Triangle[1, ncol(glmReserve$Triangle)]
+  first_ay <- c("Latest" = latest_first_ay, 1, "Ultimate" = latest_first_ay, "IBNR" = 0, "S.E." = NA, "CV" = NA)
+  xhbt <- rbind(first_ay, xhbt)
+  xhbt[, c(1, 3, 4, 5)] <- format(round(xhbt[, c(1, 3, 4, 5)], 0), big.mark = ",")
+  xhbt[, c(2, 6)] <- round(xhbt[, c(2, 6)], 3)
   xhbt
 }
