571dfa2ac55c6d6c1ee90541c550e339cbad7388
merlinoa <merlinoa88@gmail.com>
2014-10-08 20:17:59 -0700

    ready to implement reshape2

--- R/incremental.R
+++ R/incremental.R
@@ -9,14 +9,18 @@
 #' increamental(recover_ldf, value = "paid")
 cum2incr <- function(ldf, value) {
   # extract columns for transformation
-  incr <- get_col(df = ldf, type = c("id", "dev", value))
+  if (length(value) != 1) stop("value must be of length 1")
+  cum <- select_value(ldf, value)
+  incr <- data.frame(get_col(ldf_data, type = c("id", "dev")), cum)
+  incr <- carry_attr(ldf, ldf2 = incr)
   
-  incr <- dcast(, claim.number.2 ~ dev, value.var = value)
-
+  # use reshape2 to cast the cumulative values
+  incr2 <- dcast(incr, get_colname(ldf, "id") ~ get_colname(ldf, "dev"),
+                value.var = names(incr)[3])
 
   # identify oldest calendar period
   # incremental not to be calculated if a record of the origin period has not been maintained
   # since the origin period.
   min(ldf$calendar)
-
+  incr
 }
