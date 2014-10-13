d68c47c5c84f871380b68efb37c6ad3fd8c3b4ef
merlinoa <merlinoa88@gmail.com>
2014-10-12 17:37:24 -0700

    removed num_to_name and function name changes

--- R/utils.R
+++ R/utils.R
@@ -5,22 +5,12 @@ detail <- list(dollar = list(
                   recovery = c("paid_recovery", "incurred_recovery")),
                "desc"
               )
-        
-# convert column numbers to column names
-num_to_name <- function(ldf, col_num) {
-  if (!missing(col_num) && is.numeric(col_num)) {
-    col_name <- names(ldf)[col_num]
-  } else {
-    col_name <- col_num
-  }
-  col_name
-}
 
 # return columns for column names or by sum of dollar column
 # can return multiple columns
 # ldf_select(ldf_data, c("origin", "dev", "paid"))
 # ldf_select(ldf_data, c("paid_excess250", "sal_sub", "paid"))
-ldf_select <- function(ldf, values) {
+select_ldf <- function(ldf, values) {
   # return summed type for dollar detail categories
   dollar_detail <- intersect(values, unlist(detail$dollar))
   if (length(dollar_detail) > 0) {
@@ -46,7 +36,7 @@ ldf_select <- function(ldf, values) {
 #' @param df
 #' @param by vector of column names to merge by
 #' @param columns to be excluded from the merge
-merge_loss_df <- function(df, calendar1, calendar2, by, exclude) {
+merge_ldf <- function(df, calendar1, calendar2, by, exclude) {
   group1 <- df[df$calendar == calendar1, setdiff(names(df), exclude)]
   group2 <- df[df$calendar == calendar2, setdiff(names(df), exclude)]
   comparison <- merge(group1, group2, by = by,
