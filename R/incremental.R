11f51449d5dd2ca39e5bffc5c5ec518e191c5f03
merlinoa <merlinoa88@gmail.com>
2014-10-12 17:58:01 -0700

    improved checks and documentation

--- R/incremental.R
+++ R/incremental.R
@@ -1,26 +1,32 @@
 #' Transforms cumulative loss payments into incrmental
 #' 
 #' @param ldf object of class loss_df
-#' @param col column to transform from cumulative to incremental
+#' @param detail column to transform from cumulative to incremental. Can be of any of
+#' the allowable `detail` types for class `loss_df`
 #' 
 #' @export
 #' 
 #' @examples
-#' increamental(recover_ldf, value = "paid")
+#' incremental(recover_ldf, detail = "paid")
 cum2incr <- function(ldf, detail) {
-  # extract columns for transformation
+  if (is.loss_df(ldf)) stop("`ldf` must be of class `loss_df`")
   if (length(detail) != 1) stop("value must be of length 1")
+  
+  # extract columns for transformation to incremental
   cum <- select_ldf(ldf, values = c("id", "origin", "dev", detail))
   
   # use reshape2 to cast the cumulative values
   cum2 <- dcast(cum, id + origin ~ dev, sum)
   
-  # 
+  # calculate incremental losses
   incr <- t(apply(data.frame(rep(0, nrow(cum2)), cum2[, 3:length(cum2)]), 1, diff))
   
+  # transform incremetnal data frame
   incr <- data.frame(cum2[, 1:2], incr)
   names(incr) <- c("id", "origin", 1:(length(incr) - 2))
   name_incr <- paste0(detail, "_incr")
+  
+  # cast incremental data frame into long form
   incr2 <- melt(incr, id.vars = c("id", "origin"), variable.name = "dev", 
                 value.name = name_incr)
   
