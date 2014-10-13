c7f62646dcd93e56d5d699d076c629b0a14ca44c
merlinoa <merlinoa88@gmail.com>
2014-10-12 17:36:52 -0700

    first working draft of function.  Needs cleaning.

--- R/incremental.R
+++ R/incremental.R
@@ -7,20 +7,26 @@
 #' 
 #' @examples
 #' increamental(recover_ldf, value = "paid")
-cum2incr <- function(ldf, value) {
+cum2incr <- function(ldf, detail) {
   # extract columns for transformation
-  if (length(value) != 1) stop("value must be of length 1")
-  cum <- select_value(ldf, value)
-  incr <- data.frame(get_col(ldf_data, type = c("id", "dev")), cum)
-  incr <- carry_attr(ldf, ldf2 = incr)
+  if (length(detail) != 1) stop("value must be of length 1")
+  cum <- select_ldf(ldf, values = c("id", "origin", "dev", detail))
   
   # use reshape2 to cast the cumulative values
-  incr2 <- dcast(incr, get_colname(ldf, "id") ~ get_colname(ldf, "dev"),
-                value.var = names(incr)[3])
-
-  # identify oldest calendar period
-  # incremental not to be calculated if a record of the origin period has not been maintained
-  # since the origin period.
-  min(ldf$calendar)
-  incr
+  cum2 <- dcast(cum, id + origin ~ dev, sum)
+  
+  # 
+  incr <- t(apply(data.frame(rep(0, nrow(cum2)), cum2[, 3:length(cum2)]), 1, diff))
+  
+  incr <- data.frame(cum2[, 1:2], incr)
+  names(incr) <- c("id", "origin", 1:(length(incr) - 2))
+  name_incr <- paste0(detail, "_incr")
+  incr2 <- melt(incr, id.vars = c("id", "origin"), variable.name = "dev", 
+                value.name = name_incr)
+  
+  # remove all times greater than the most recent calendar period  
+  incr2$dev <- as.numeric(as.character(incr2$dev))
+  incr2 <- incr2[(incr2$origin + incr2$dev) <= max(ldf$calendar), ]
+  out <- merge(ldf, incr2, by = c("id", "origin", "dev"))
+  out[, c("id", "origin", "dev", name_incr)]
 }
